0cf1e8c47ee754e6988990f12a5cad3601c87552
Daniel Melo Alencar <danielmalencar2011@gmail.com>
2021-09-16 05:29:41 -0300

    Adicionado splitpanes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <splitpanes
+    horizontal
+    :push-other-panes="false"
+    style="height: 97vh;margin:0px padding:0px"
+  >
+    <pane min-size="5" size="5" max-size="5" style="display: flex">
+      <div style="display: flex; flex: 1">
+        <div
+          style="
+            display: flex;
+            margin-right: 10px;
+            width: 150px;
+            align-items: center;
+            justify-content: center;
+          "
+        >
+          <span>EgleIDE</span>
+        </div>
+        <div style="display:flex; flex: 1; align-items: center; justify-content: start">
+          Menu começa aqui
+        </div>
+      </div>
+    </pane>
+    <pane>
+      <splitpanes :push-other-panes="false">
+        <pane min-size="5" size="15">
+          <span>Propriedades</span>
+        </pane>
+        <pane min-size="5" size="70">
+          <span>Conteudo </span>
+        </pane>
+        <pane min-size="5" size="15">
+          <span>Componentes</span>
+        </pane>
+      </splitpanes>
+    </pane>
+    <pane min-size="5" size="10">
+      <span>Console</span>
+    </pane>
+  </splitpanes>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//https://antoniandre.github.io/splitpanes
 
+import { Splitpanes, Pane } from "splitpanes";
+import "splitpanes/dist/splitpanes.css";
+import "./css-reset.css";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Splitpanes,
+    Pane,
+  },
+};
 </script>
 
 <style>
+body {
+  background-color: #21222c;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+
+.splitpanes {
+  background-color: #21222c;
+  color: #fff;
+}
+
+.splitpanes__splitter {
+  background-color: #6272a4;
+  position: relative;
+}
+.splitpanes__splitter:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  transition: opacity 0.4s;
+  background-color: rgba(98, 114, 164, 0.8);
+  opacity: 0;
+  z-index: 1;
+}
+.splitpanes__splitter:hover:before {
+  opacity: 1;
+}
+.splitpanes--vertical > .splitpanes__splitter:before {
+  left: -5px;
+  right: -5px;
+  height: 100%;
+}
+.splitpanes--horizontal > .splitpanes__splitter:before {
+  top: -5px;
+  bottom: -5px;
+  width: 100%;
 }
 </style>
